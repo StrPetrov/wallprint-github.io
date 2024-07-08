02e0c3ce03287ed849fe1eefd872ad8952d90c32
StrPetrov <strpet7@gmail.com>
2024-07-08 20:40:43 +0200

    Animations and button looks.

--- src/app/components/price-tag/price-tag.component.ts
+++ src/app/components/price-tag/price-tag.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-price-tag',
@@ -10,4 +10,19 @@ export class PriceTagComponent {
  @Input() priceValue!: string;
  @Input() printingSurficeFrom!: string;
  @Input() printingSurficeTo!: string;
+
+ @ViewChild('overlayAnimation') overlayAnimation!: ElementRef;
+ @ViewChild('priceTag') priceTag!: ElementRef;
+
+ constructor(private renderer: Renderer2) {}
+
+ triggerAnimation = () => {
+  this.renderer.addClass(this.overlayAnimation.nativeElement, 'animate-background');
+  this.renderer.addClass(this.priceTag.nativeElement, 'animate-price-tag');
+
+  setTimeout(() => {
+    this.renderer.removeClass(this.overlayAnimation.nativeElement, 'animate-background');
+    this.renderer.removeClass(this.priceTag.nativeElement, 'animate-price-tag');
+  }, 500)
+ }
 }
