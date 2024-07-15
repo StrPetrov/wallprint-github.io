50e13a17973ceb4cd91721056d523d4e3b995ab8
StrPetrov <strpet7@gmail.com>
2024-07-15 10:57:12 +0200

    form error handling.

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,11 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { SharedService } from 'src/app/services/shared.service';
@@ -6,56 +13,68 @@ import { SharedService } from 'src/app/services/shared.service';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit, AfterViewInit, OnDestroy {
   isMailSending = false;
   isMailSent = false;
 
-  contactForm!: FormGroup
+  contactForm!: FormGroup;
 
-  subscription!: Subscription
+  subscription!: Subscription;
 
-  @ViewChild('formRef') formWrapper!: ElementRef
+  @ViewChild('formRef') formWrapper!: ElementRef;
 
   constructor(private sharedService: SharedService) {}
 
   ngOnInit(): void {
-      this.contactForm = new FormGroup({
-        firstName: new FormControl(null, Validators.required),
-        lastName: new FormControl(null, Validators.required),
-        adress: new FormControl(null, Validators.required),
-        phoneNumber: new FormControl(null, Validators.required),
-        surfice: new FormControl(null, Validators.required),
-        email: new FormControl(null, [Validators.required, Validators.email]),
-        other: new FormControl(null)
-      })
+    this.contactForm = new FormGroup({
+      firstName: new FormControl(null, Validators.required),
+      lastName: new FormControl(null, Validators.required),
+      adress: new FormControl(null, Validators.required),
+      phoneNumber: new FormControl(null, Validators.required),
+      surfice: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      other: new FormControl(null),
+    });
   }
 
   ngAfterViewInit(): void {
-      this.subscription = this.sharedService.scrolledDownFormSubject.subscribe(() => {
-        this.formWrapper.nativeElement.scrollIntoView({ behaviour: 'smooth', block: 'start'} )
-      })
+    this.subscription = this.sharedService.scrolledDownFormSubject.subscribe(
+      () => {
+        this.formWrapper.nativeElement.scrollIntoView({
+          behaviour: 'smooth',
+          block: 'start',
+        });
+      }
+    );
   }
 
   submit = () => {
     const formData = this.contactForm.value;
     this.contactForm.reset();
-    
+
     this.isMailSending = true;
 
-    this.sharedService.sendMail(formData).subscribe((data: any) => {
-      console.log(data);
-      this.isMailSending = false
-      this.isMailSent = true;
-
-      setTimeout(() => {
-        this.isMailSent = false;
-      }, 2000)
-    })
-  }
+    this.sharedService.sendMail(formData).subscribe({
+      next: (data: any) => {
+        console.log(data);
+        this.isMailSending = false;
+        this.isMailSent = true;
+    
+        setTimeout(() => {
+          this.isMailSent = false;
+        }, 2000);
+      },
+      error: (error: any) => {
+        console.log(error);
+        this.isMailSending = false;
+        this.isMailSent = false;    
+      }
+    });
+  };
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe()
+    this.subscription.unsubscribe();
   }
 }
